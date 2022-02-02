550789c30f5c5ccef93cd9927ca606e7e85b445e
Bilal Essalhi <bilal.essalhi@student.rocva.nl>
2022-02-02 14:08:27 +0100

    klaar

---
+++ 3-Arrays/3.2 - Array waarde wijzigen/index.php
@@ -0,0 +1,11 @@
+<?php
+$myDogNames = [ "Pitbull" , "Akita" , "Duitse Herder" ];
+echo $myDogNames[0] . "<br>";
+echo $myDogNames[1] . "<br>";
+echo $myDogNames[2] . "<br><br>" ;
+
+$myDogNames[0] = 'Demitridemarcusbartholomowjuniorthethird';
+echo $myDogNames[0] . "<br>";
+echo $myDogNames[1] . "<br>";
+echo $myDogNames[2] . "<br>";
+?>
