e95fa01a77a3801c43afe0710e38aa2844a45891
Bilal Essalhi <bilal.essalhi@student.rocva.nl>
2022-02-02 14:16:07 +0100

    done

---
+++ 3-Arrays/3.3 - Array waarde toevoegen/index.php
@@ -0,0 +1,9 @@
+<?php
+$myHeroes = ['flash' , 'Spiderman' , 'arrow'];
+
+// echo $myHeroes[0] . "<br>";
+// echo $myHeroes[1] . "<br>";
+// echo $myHeroes[2] . "<br>";
+
+array_push($myHeroes, 'GT');
+print_r($myHeroes); 
