3bd9e1fe6df7aa8548bfc08d1b704dd1d0f9de25
Bilal Essalhi <bilal.essalhi@student.rocva.nl>
2022-02-02 13:17:09 +0100

    FINISHED

---
+++ 2-Variabelen-en-datatype/2.8 - data en html combineren/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+$voornaam1 = "Bilal";
+$voornaam2 = "jorik";
+$voornaam3 = "Jonna";
+echo "<ul> <li>$voornaam1 </li> <li> $voornaam2</li>  <li>$voornaam3</li> <ul>";
+?>
+</body>
+</html>
