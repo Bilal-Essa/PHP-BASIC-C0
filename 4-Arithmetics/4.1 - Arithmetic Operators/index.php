7c914523100e31542f70554122a35dd4e2f9398c
Bilal Essalhi <bilal.essalhi@student.rocva.nl>
2022-02-03 11:02:06 +0100

    done

---
+++ 4-Arithmetics/4.1 - Arithmetic Operators/index.php
@@ -0,0 +1,10 @@
+<?php
+$getal1= 12;
+$getal2 = 3;
+echo $getal1 + $getal2 . " ";
+echo "<br>";
+echo $getal1 - $getal2 . " ";
+echo "<br>";
+echo $getal1 * $getal2 . " ";
+echo "<br>";
+echo $getal1 / $getal2;
