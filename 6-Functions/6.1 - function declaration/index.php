bdf43977046c8f0738bf1639855e54a6aa5a68b8
Bilal Essalhi <bilal.essalhi@student.rocva.nl>
2022-02-03 14:23:01 +0100

    done

---
+++ 6-Functions/6.1 - function declaration/index.php
@@ -0,0 +1,12 @@
+<?php
+
+function zetTweeWoordenAanElkaar(){
+ $woordje = "hallo ";
+ $zinnetje = 'mooie blauwe planeet!';
+ $heleZin = $woordje . $zinnetje; 
+ echo $heleZin;
+}
+
+zetTweeWoordenAanElkaar(); 
+
+?>
